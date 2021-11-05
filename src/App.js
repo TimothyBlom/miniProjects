9fe024ea49aa571a179f258e004e239dfaa48328
kracok <kracok@gmail.com>
2021-11-05 17:45:25 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import Logo from "./images/3DLogo.png";
+import React, {useEffect, useState} from 'react';
 import './style/App.css';
+import Logo from "./images/3DLogo.png";
+import boxArt from './images/boxArt/ratchet1box.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Yes, my logo. Rotate, rotate FOREVER.
-        </p>
-      </header>
+
+    <div className="Header">
+      <img src={Logo} className="headerLogo" alt="" />
+    </div>
+
+      <div className="exampleContainer">
+        <img src={boxArt} className="boxButton" alt="" />
+      </div>
+
     </div>
   );
 }
